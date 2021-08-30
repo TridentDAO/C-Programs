fffa4260dbaf9fb6486d3b80c3f9660894f50171
TridentDAO <85838743+TridentDAO@users.noreply.github.com>
2021-08-30 11:06:49 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,21 @@
+// First C Program
+#include<stdio.h>
+void main()
+{
+    printf("Hello World");
+}
+
+
+
+
+
+
+
+
+
+// This how you can write comments in program
+// Comment will not be executed by compiler
+/*
+Multiline
+Comment
+*/
